7acee3932fffc70bf3c6bfa54809cc7112ef7a35
borchr27 <30349201+borchr27@users.noreply.github.com>
2019-12-16 12:02:12 -0500

    added function for determining which player

--- Chips/main.cpp
+++ Chips/main.cpp
@@ -1,6 +1,7 @@
 //Hello World
 //Mitchell B
 // link to helpful video: https://www.youtube.com/watch?v=DIw02CaEusY
+// linkedin learning link https://www.linkedin.com/learning/learning-c-plus-plus-3/using-functions-in-our-program
 
 // need to save code crtl-s, build code, crtl-shft-b, then execute with .\a.exe
 #include <iostream>
